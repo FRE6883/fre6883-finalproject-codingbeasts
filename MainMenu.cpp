e3e50c9d41e046cccf96e80ac517358dd4212175
EC2 Default User <ec2-user@ip-172-31-50-225.ec2.internal>
2023-12-15 06:03:14 +0000

    fre6883-finalproject-codingbeasts

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -16,6 +16,44 @@
 int main() {
 
     int N = 60;
-
-    return 0;
-}
+    
+    cout << "\n\nMenu: " << endl;
+    cout << "1) Enter N to retrieve 2N+1 historical prices for all stocks" << endl;
+    cout << "2) Pull information for one stock from one group. " << endl;
+    cout << "3) Display AAR, AAR-std, CAAR, CAAR-std for one group. " << endl;
+    cout << "4) Display gnuplot plot for CAAR of all 3 groups.  " << endl;
+    cout << "5) Exit program " << endl << endl;
+    cout << "Please select appropriate option number:  " << endl;
+    cin >> x;
+    
+    switch(x) {
+        case 1: {
+            // Your code for option 1
+            break;
+        }
+        case 2: {
+            // Your code for option 2
+            break;
+        }
+        case 3: {
+            // Your code for option 3
+            break;
+        }
+        case 4: {
+            // Your code for option 4
+            break;
+        }
+        case 5: {
+            exit(0);
+            break;
+        }
+        default: {
+            cout << "Error, Please select valid option " << endl;
+            cin.clear();
+            break;
+        }
+    }
+    
+    
+        return 0;
+    }
